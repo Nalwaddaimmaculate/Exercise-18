83704ce89ba1fc83a8223e16f8a31bd778bc532c
Nalwaddaimmaculate <117144141+Nalwaddaimmaculate@users.noreply.github.com>
2023-07-02 23:28:36 -0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,25 @@
+let {useState} = React;
+let MyCounter = () => {
+
+    let [counter,setCounter] = useState(27) ; 
+
+
+    
+    let clickHandler = () => {
+        counter++;
+        setCounter(counter);
+        console.log(counter)
+    }
+
+    
+    return(
+        <div>
+            <button onClick={clickHandler}>
+            {counter}! </button>
+            
+        </div>
+    );
+}
+
+let root = ReactDOM.createRoot(document.getElementById("mydiv"));
+root.render(<MyCounter/>)
